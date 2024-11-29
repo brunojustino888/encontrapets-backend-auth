ca6e2d75c032891ff37a6558a110b1daa7cc52c1
Bruno Alves Justino <brunojustino@gmail.com>
2024-11-29 08:04:09 -0300

    Cors again.

--- src/main/java/br/com/encontrapets/config/CorsConfig.java
+++ src/main/java/br/com/encontrapets/config/CorsConfig.java
@@ -27,12 +27,10 @@ public class CorsConfig {
 	 */
     @Bean
     public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("https://encontrapets.com.br");
+    	CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173 , https://encontrapets.com.br");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
