ecb9c0f2d232b796d1ac27e07491ecb7307ef2be
Bruno Alves Justino <brunojustino@gmail.com>
2024-11-29 07:53:10 -0300

    Cors.

--- src/main/java/br/com/encontrapets/config/CorsConfig.java
+++ src/main/java/br/com/encontrapets/config/CorsConfig.java
@@ -1,24 +1,41 @@
 package br.com.encontrapets.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+/**
+ * Classe de configuracao CORS das requisicoes.
+ * 
+ * @author Bruno Justino. 
+ */
 @Configuration
 public class CorsConfig {
-    
+	
+	/**
+	 * Configuracoes de origens e metodos CORS.
+	 * Permitidas as origens localhost e dominio encontrapets.
+	 * Permite todos os cabeçalhos.
+	 * Permite envio de cookies ou credenciais.
+	 * Aplica a configuração para todas as rotas.
+	 * 
+	 * @return CorsFilter - filtro CORS customizado.
+	 */
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://encontrapets.com.br","encontrapets.com.br")  
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://encontrapets.com.br");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
+    
 }
